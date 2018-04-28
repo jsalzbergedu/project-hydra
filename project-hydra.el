94dc9a85f82d4bdd020913c4770bcd3b65e98e6f
Jacob Salzberg <jssalzbe@ncsu.edu>
2018-04-28 16:57:54 -0400

    Add docstring generator, format it properly

--- project-hydra.el
+++ project-hydra.el
@@ -1,4 +1,5 @@
-;; -*- lexical-binding: t -*-
+;;; project-hydra --- hyrda skeleton for disparate modes -*- lexical-binding: t -*-
+;;; Commentary:
 ;; A hydra skeleton for projects.
 
 ;; Will support mainly operations on whole projects, such as:
@@ -26,17 +27,52 @@
 ;; (project-hydra
 ;;  :test test-fun)
 
+;; Full example
+;; 
+;; (defun sillyfun ()
+;;   (interactive)
+;;   (message "hi!"))
+
+;; (project-hydra doot-hydra
+;;   :test sillyfun
+;;   :build sillyfun
+;;   :run sillyfun
+;;   :stylecheck sillyfun
+;;   :and ("v" sillyfun))
+;;
+;;
+;; Becomes
+;;
+;; (defhydra doot-hydra
+;;   (:color blue :hint nil)
+;;   "
+;; ^Project Operations and Tasks^
+;; ----------------------------
+;; _t_ sillyfun
+;; _b_ sillyfun
+;; _r_ sillyfun
+;; _s_ sillyfun
+;; _v_ sillyfun
+;; "
+;;   ("t" sillyfun)
+;;   ("b" sillyfun)
+;;   ("r" sillyfun)
+;;   ("s" sillyfun)
+;;   ("v" sillyfun))
+
+;;; Code:
+
 (require 'hydra)
-(require 'cl-seq)
+(require 'cl-lib)
 
 (defun project-hydra--general-rule (sym key)
-  "The general rule: (:sym . key) which will be applied to sexp"
+  "The general rule for (:SYM . KEY), used in code generation for the keybinding."
   (lambda (sexp)
     (when (eq (car sexp) sym)
       (list key (cadr sexp)))))
 
 (defun project-hydra--general-rule-list (list)
-  "The unapplied version of project-hydra--gerneral-rule"
+  "The unapplied version of project-hydra--gerneral-rule, applied to LIST."
   (apply 'project-hydra--general-rule list))
 
 (defvar project-hydra-rules
@@ -46,25 +82,24 @@
     (:stylecheck "s"))
   "The rules that bind a key (in the symbol sense) (e.g. :test) to a key (in the keyboard sense).")
 
-
 (defun project-hydra--and-rule (sexp)
-  "The rule for :add (it simply is not touched)"
+  "The rule for :add (SEXP is simply not touched)."
   (when (eq (car sexp) ':and)
     (cadr sexp)))
 
-(message "%s" (append '(1 2) '(1)))
-
 (defun project-hydra--parse-one-sexp (sexp)
-  "Parse one sexp from the body"
-  (let* ((normal-rules (map 'list 'project-hydra--general-rule-list project-hydra-rules))
+  "Parse one SEXP from the body."
+  (let* ((normal-rules (cl-map 'list 'project-hydra--general-rule-list project-hydra-rules))
 	 (rules (cons 'project-hydra--and-rule normal-rules)))
-    (car (cl-remove-if-not 'identity (map 'list (lambda (a) (funcall a sexp)) rules)))))
+    (message "%s" rules)
+    (car (cl-remove-if-not 'identity (cl-map 'list (lambda (a) (funcall a sexp)) rules)))))
 
-(defun pair-off (list)
+(defun project-hydra--pair-off (list)
+  "Pair off every two elements in LIST."
   (when (= (% (length list) 2) 0)
     (let ((flag t)
 	  (acc '())
-	  (list list)) 
+	  (list list))
       (while flag
 	(setq acc (append acc (list (list (car list) (cadr list)))))
 	(setq list (cddr list))
@@ -73,32 +108,41 @@
       acc)))
 
 (defun project-hydra--parse-body (body)
-  "Parse the body of the macro"
-  (map 'list 'project-hydra--parse-one-sexp (pair-off body)))
+  "Parse the BODY of the macro."
+  (cl-map 'list 'project-hydra--parse-one-sexp (project-hydra--pair-off body)))
 
 (defun project-hydra--begin-body (name)
+  "Begin the body of the hydra, taking argument NAME."
   `(defhydra ,name (:color blue :hint nil)))
 
+(defun project-hydra--rightpad (s len)
+  "Right pad S with LEN spaces."
+  (concat s (cl-loop repeat len concat " ")))
+
+(defun project-hydra--docstring-rule (sexp)
+  "Generate a docstring from SEXP of the parsed results of project-hydra--parse-body."
+  (format "_%s_ %s\n" (car sexp) (cadr sexp)))
+
+(defun project-hydra--gen-docstring (body)
+  "Generate a docstring from the parsed results of BODY."
+  (list (cl-reduce 'concat (cl-map 'list 'project-hydra--docstring-rule body)
+		   :initial-value (concat "\n"
+					  "^Project Operations and Tasks^\n"
+					  "----------------------------\n"))))
+
 (defmacro project-hydra (name &rest body)
-  "A template for generating hydras for projects in different languages.
+  "A template for generating for projects in different languages.
+NAME is used for the name, and BODY is parsed into the hydra's body.
 To use, either use one of the keys defined in project-hydra-rules like this:
-(project-hydra :test test-fun
+\(project-hydra :test test-fun
 :build build-fun
 :run run-fun
 :stylecheck stylecheck-fun
-:and (key . other-binding)
-:and (key . and-so-on))
-"
+:and \(key . other-binding\)
+:and \(key . and-so-on\)\)"
   (declare (indent defun))
-  (append (project-hydra--begin-body name) (project-hydra--parse-body body)))
+  (let ((parsed (project-hydra--parse-body body)))
+    (append (project-hydra--begin-body name) (project-hydra--gen-docstring parsed) parsed)))
 
-(defun sillyfun ()
-  (interactive)
-  (message "hi!"))
-
-(project-hydra doot-hydra
-  :test sillyfun
-  :build sillyfun
-  :run sillyfun
-  :stylecheck sillyfun
-  :and ("v" sillyfun))
+(provide 'project-hydra)
+;;; project-hydra.el ends here
